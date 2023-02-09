3a3747fa1688823f3531da1e90c2116c871df971
josecas <jmcg0901@gmail.com>
2023-02-08 21:36:15 -0600

    Place entities based on taps

--- ARPersistencySwiftUI/ViewControllers/ARViewController.swift
+++ ARPersistencySwiftUI/ViewControllers/ARViewController.swift
@@ -8,46 +8,60 @@
 import UIKit
 import ARKit
 import RealityKit
+import Combine
 
 #if !targetEnvironment(simulator)
 class ARViewController: UIViewController, ARSessionDelegate {
-
+    
     @IBOutlet var arView: ARView!
+    var model = Entity()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        placeModel()
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        arView.session.run(config)
+        self.model = try! Entity.load(named: "toy_drummer")
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        arView.addGestureRecognizer(tapGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
-    private func placeModel() {
-        guard let drummer = try? Entity.load(named: "toy_drummer") else { return }
-        let anchor = AnchorEntity(plane: .horizontal)
-        anchor.addChild(drummer)
-        arView.scene.anchors.append(anchor)
-        for animation in drummer.availableAnimations {
-            drummer.playAnimation(animation.repeat())
-        }
+    var raycastAnchors: [AnchorEntity] = []
+
+    @objc func handleTap(gesture: UITapGestureRecognizer) {
+        guard let query = arView.makeRaycastQuery(from: gesture.location(in: self.arView), allowing: .existingPlaneInfinite, alignment: .any)
+        else { return }
+        
+        guard let result = arView.session.raycast(query).first
+                
+        else { return }
+        
+        let newModel = model.clone(recursive: true)
+        let raycastAnchor = AnchorEntity(world: result.worldTransform)
+        raycastAnchor.addChild(newModel)
+        arView.scene.addAnchor(raycastAnchor)
     }
-    
+
+
 }
 
 extension ARViewController {
-
+    
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         print("didAdd")
     }
-
+    
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         
     }
-
+    
     func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
         print("didRemove")
     }
-
+    
 }
 #endif
